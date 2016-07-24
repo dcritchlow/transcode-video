70221578e3bf022c5bd14395cbd944fb13940098
Darin Critchlow <darincritchlow@gmail.com>
2016-07-23 23:32:57 -0600

    Finalize batch.ps1 with proper format
    Put batch.ps1 script into seperate functions and add commands that can be used with Get-Help command

--- batch.ps1
+++ batch.ps1
@@ -1,41 +1,214 @@
+#requires -version 4
+
+<#
+.SYNOPSIS
+  Batch execution for transcoding video .mkv files to .mp4 for plex server
+.DESCRIPTION
+  Powershell script that works with .mkv video files and transcodes them to .mp4 for plex servers
+.PARAMETER DeleteFile
+  Optional. When parameter specified it will remove the original video file(s)
+.INPUTS
+  Parameters above
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         Darin Critchlow
+  Creation Date:  07/20/2016
+  Purpose/Change: Initial script development
+.EXAMPLE
+  ./batch.ps1
+
+  Starts processing video(s) listed in the queue.txt file and creates a new .mp4 video file in the completed directory inside a new
+  directory with the same name that is in queue.txt file.
+
+.EXAMPLE
+  ./batch.ps1 -DeleteFile
+  
+  Starts processing video(s) listed in the queue.txt file and creates a new .mp4 video file in the completed directory inside a new
+  directory with the same name that is in queue.txt file and removes original .mkv video file.
+
+.LINK
+  https://github.com/dcritchlow/transcode-video
+
+#>
+
+#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+
+Param (
+  [Parameter(Mandatory=$false,Position=0)][switch]$DeleteFile
+)
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Global Declarations
 $currDir = Get-Location
 $completed = "$currDir\Completed"
 $queue = "$currDir\queue.txt"
 $crops = "$currDir\Crops"
-$logs = "$completed\Logs"
-$videoLocation = "C:\Video"
+$logs = "$currDir\TranscodeLogs"
+$videoLocation = "$currDir\Videos"
 
-$lines = Get-Content $queue
-Write-Host $lines
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-foreach($line in $lines){
+Function Start-VideoProcess {
+  Param ([string[]] $lines)
+  Begin {
+    Write-Host 'Processing the next movie in queue...'
+  }
+  Process {
+    Try {
 
-    $video = "$videoLocation\$line\$line.mkv"
+        $lines = Get-VideoQueue
+        
+        foreach($line in $lines) {
 
-    $cropFile = "$crops\$fileName.txt"
-    if(Test-Path $cropFile){
-        $cropValue = Get-Content $cropFile
-        $crop_option = "--crop $cropValue"
-    } else {
-        $crop_option = ''
+            $video = "$videoLocation\$line\$line.mkv"
+            Write-Host $video
+
+            $cropOption = Get-CropFile($line)
+
+            # Create Directory for movie file
+            Add-MovieDirectory($line)
+            
+            # Build Command
+            $command = "transcode-video.bat --mp4 -o ""$completed\$line\$line.mp4"" $cropOption ""$video"""
+
+            # Run command
+            iex "&$command"
+
+            # Move log file to logs directory
+            mv "$completed\$line\$line.mp4.log" $logs
+
+            #Deletes original .mkv file
+            If ( $DeleteFile -eq $True ) {
+                Remove-Item $video
+            }
+        }
+
+        # Remove line(s) from queue.txt file
+        Clear-QueueFile
+
     }
-
-    # Create Directory for movie file
-    ni "$completed\$line" -type Directory
-    
-    # Build Command
-    $command = "transcode-video.bat --mp4 -o ""$completed\$line\$line.mp4"" $crop_option ""$video"""
-    
-    
-    # Run command
-    iex "&$command"
-
-    # Move log file to logs directory
-    mv "$completed\$line\$line.mp4.log" $logs
-
-    # This removes actual file
-    # Remove-Item $line
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed video transcoding.'
+      Write-Host ' '
+    }
+  }
 }
 
-# Remove line(s) from queue.txt file
-Out-File $queue -Force
+Function Get-VideoQueue {
+  Param ()
+  Begin {
+    Write-Host 'Getting the list of movies to process...'
+  }
+  Process {
+    Try {
+      $lines = Get-Content $queue
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed getting video queue.'
+      Write-Host ' '
+      Return $lines
+    }
+  }
+}
+
+Function Get-CropFile {
+  Param ([String] $line)
+  Begin {
+    Write-Host 'Checking for a Crop file for video...'
+  }
+  Process {
+    Try {
+        $cropFile = "$crops\$line.txt"
+        if(Test-Path $cropFile){
+            Write-Host 'Found a crop file setting value for command...'
+            $cropValue = Get-Content $cropFile
+            $cropOption = "--crop $cropValue"
+        } else {
+            Write-Host 'No crop file found...'
+            $cropOption = ''
+        }
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed getting crop file.'
+      Write-Host ' '
+      Return $cropOption
+    }
+  }
+}
+
+
+Function Add-MovieDirectory {
+  Param ([String] $line)
+  Begin {
+    Write-Host 'Creating directory to hold video file...'
+  }
+  Process {
+    Try {
+      ni "$completed\$line" -type Directory
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Created Directory.'
+      Write-Host ' '
+    }
+  }
+}
+
+Function Clear-QueueFile {
+  Param ()
+  Begin {
+    Write-Host 'Clearing queue.txt file...'
+  }
+  Process {
+    Try {
+      Out-File $queue -Force
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'File is empty.'
+      Write-Host ' '
+    }
+  }
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Start-VideoProcess
